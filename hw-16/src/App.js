e2b948613e46d77450d248ab6701f468b1400451
AnastasiK30 <anastasia301284@gmail.com>
2025-06-27 23:03:39 +0200

    added homework 16

---
+++ hw-16/src/App.js
@@ -0,0 +1,81 @@
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import { useState } from 'react'
+import Button from '@mui/material/Button'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+
+import styles from './App.module.css'
+
+function App() {
+  const [open, setOpen] = useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+  return (
+    <div className={styles.app}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              My Material UI App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <div className={styles.container}>
+        <Typography variant="h2">
+          Wellcome to our App!!!
+        </Typography>
+
+        <Button style={{marginTop: '20px'}} variant="contained" onClick={handleClickOpen}>
+          Open dialog window
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {'Use Material UI Library?'}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Test application using library Material UI
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button variant='contained' onClick={handleClose}>Disagree</Button>
+            <Button variant='contained' onClick={handleClose} autoFocus>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    </div>
+  )
+}
+
+export default App
